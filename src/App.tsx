c9e006b6636cffd5440213e9c8335b503a0e4ed5
iDevYT <alphangred57@gmail.com>
2022-08-14 18:42:06 +0200

    added tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -29,7 +29,7 @@ function App() {
   return (
         <div className="App">
           <header>
-            <h1>Polygon Social</h1>
+            <h1 className="text-3xl">Polygon Social</h1>
             <a href-='mailto://alphangred57@gmail.com'>Contact</a> <br/>
             <SignOut/>
           </header>
