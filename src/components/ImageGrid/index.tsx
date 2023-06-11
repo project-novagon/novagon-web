569655bbd82b7d81ee2b8d0bef11b48da50fbac2
iDevYT <alphangred57@gmail.com>
2023-06-11 20:47:45 +0200

    lmao

--- src/components/ImageGrid/index.tsx
+++ src/components/ImageGrid/index.tsx
@@ -1,65 +1,78 @@
 import { getDownloadURL } from "firebase/storage";
-import { collection, doc, getDocs } from "firebase/firestore";
-import React, { useEffect, useState } from "react";
 import { fetchAllImages } from "../../services";
-import { db } from "../../firebase-config"; // Import the db instance
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+import React, { useEffect, useState } from "react";
+import { auth } from "../../firebase-config";
+
+interface User {
+  username: string;
+  photoURL: string;
+}
 
 export const ImageGrid = () => {
   const [imgs, setImgs] = useState<string[]>([]);
-  const [users, setUsers] = useState<{ [key: string]: string }>({});
-
-  const getUsersFromFirestore = async () => {
-    try {
-      const usersRef = collection(db, "users");
-      const usersSnapshot = await getDocs(usersRef);
-      const usersData = usersSnapshot.docs.reduce((result, doc) => {
-        return { ...result, [doc.id]: doc.data().username };
-      }, {});
-      setUsers(usersData);
-    } catch (error) {
-      console.error(error);
-    }
-  };
-
-  const getUrlsFromImages = async () => {
-    try {
-      const images = await fetchAllImages();
-      images.items.forEach(async (img) => {
-        const url = await getDownloadURL(img);
-        setImgs((value) => [...value, url]);
-      });
-    } catch (error) {
-      console.error(error);
-    }
-  };
+  const [users, setUsers] = useState<{ [key: string]: User }>({});
 
   useEffect(() => {
-    getUsersFromFirestore();
+    const getUrlsFromImages = async () => {
+      try {
+        const images = await fetchAllImages();
+        const promises = images.items.map(async (img) => {
+          const url = await getDownloadURL(img);
+          return url;
+        });
+        const urls = await Promise.all(promises);
+        setImgs(urls);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
     getUrlsFromImages();
   }, []);
 
+  onAuthStateChanged(auth, (user) => {
+    if (user) {
+      const userID = user.uid;
+      const username = user.displayName || "Unknown User";
+      const photoURL = user.photoURL || "";
+      setUsers({ [userID]: { username, photoURL } });
+      console.log(username, userID, photoURL);
+    }
+  });
+  
+  
+
   return (
     <div className="grid gap-5 md:grid-cols-4 lg:grid-cols-6 sm:grid-cols-2">
-    {imgs &&
-  imgs.map((img, index) => {
-    const uid = img.split("/")[1].split("_")[0];
-    const username = users[uid] || uid; // Use uid as the fallback value
-    return (
-      <div key={index} className="dark:bg-zinc-700 rounded-lg shadow-xl">
-        <p>{username}</p>
-        <img
-          className="rounded"
-          height={200}
-          width={200}
-          src={img}
-          alt="Image"
-          loading="lazy"
-        />
-      </div>
-    );
-  })}
-  </div>
-  
-  
+      {imgs &&
+        imgs.map((img, index) => {
+          const decodedURL = decodeURIComponent(img);
+          const filename = decodedURL.split("/").pop();
+          if (filename) {
+            const [userID] = filename.split("_");
+            const { username, photoURL } = users[userID] || { username: userID, photoURL: "" };
+            return (
+              <div key={index} className="dark:bg-zinc-700 rounded-lg shadow-xl p-2 space-y-2 justify-center">
+                <div>
+                <div className="flex items-center justify-between p-2">
+                {username && <p>By {username}</p>} 
+                {photoURL && <img src={photoURL} alt={username} className="h-8 rounded-full"/>}
+                </div>
+                <img
+                  className="rounded"
+                  height={200}
+                  width={200}
+                  src={img}
+                  alt="Image"
+                  loading="lazy"
+                />
+              </div>
+            </div>
+            );
+          }
+          return null;
+        })}
+    </div>
   );
 };
