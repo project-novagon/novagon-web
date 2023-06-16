47de342208b5e1ef6bb88689ed6b580cf60cc6a1
iDevYT <alphangred57@gmail.com>
2023-06-16 20:12:00 +0200

    yes

---
+++ src/pages/chat.tsx
@@ -0,0 +1,90 @@
+import { auth, db } from "../firebase-config";
+import { doc, getDoc, Timestamp, collection, serverTimestamp, query, orderBy, DocumentData, Query } from "firebase/firestore";
+import { User } from "firebase/auth";
+import { PaperAirplaneIcon } from '@heroicons/react/24/outline'
+import { useState } from "react";
+import { useCollectionData } from 'react-firebase-hooks/firestore';
+import { useAuthState } from "react-firebase-hooks/auth";
+
+
+interface Message {
+    id: string;
+    text: string;
+    sendDate: Timestamp | Date;
+    uid: string;
+    photoURL: string;
+    displayName: string;
+  }
+
+interface ChatMessageProps {
+    message: Message;
+    currentUser: User | null;
+  }
+
+function ChatMessage(props: ChatMessageProps) {
+    const { message, currentUser } = props;
+    const { text, uid, photoURL, displayName } = message;
+    const messageClass = uid === currentUser?.uid ? 'sent' : 'received';
+  
+    return (
+      <div className={`message ${messageClass}`}>
+        <img src={photoURL} alt="User Avatar" />
+        <div>
+          <span className="display-name">{displayName}</span>
+          <p>{text}</p>
+        </div>
+      </div>
+    );
+  }
+
+  function ChatRoom() {
+    const publicChatRef = collection(db, 'messages');
+    const listAllImages = query(publicChatRef, orderBy("sendDate"))
+  
+    const [messages] = useCollectionData<Message>(listAllImages, { idField: 'id' });
+    const [formValue, setFormValue] = useState('');
+  
+    const sendMessage = async (e: React.FormEvent<HTMLFormElement>) => {
+      e.preventDefault();
+  
+      const { uid, photoURL, displayName } = auth.currentUser || {};
+  
+      await publicChatRef.add({
+        text: formValue,
+        sendDate: serverTimestamp(),
+        uid,
+        photoURL,
+        displayName,
+      });
+  
+      setFormValue('');
+    };
+  
+    return (
+      <div>
+        <main>
+          {messages &&
+            messages.map((message: Message) => (
+              <ChatMessage
+                key={message.id}
+                message={message}
+                currentUser={useAuthState(auth)}
+              />
+            ))}
+        </main>
+  
+        <form onSubmit={sendMessage}>
+          <input
+            value={formValue}
+            onChange={(e) => setFormValue(e.target.value)}
+            placeholder="Type your message..."
+          />
+          <button type="submit">
+            <PaperAirplaneIcon className="send-icon" />
+          </button>
+        </form>
+      </div>
+    );
+  }
+
+export default ChatRoom;
