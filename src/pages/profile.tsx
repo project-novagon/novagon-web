2beb2de3401a40bf548210f3080f119d35a1e949
iDevYT <alphangred57@gmail.com>
2023-08-14 18:42:50 +0200

    💩  added edit profile function

--- src/pages/profile.tsx
+++ src/pages/profile.tsx
@@ -7,7 +7,7 @@ function Profile() {
         <>
             <div className="space-y-3">
                 <h1 className="text-3xl">Hello, {user?.displayName ? <h1 className="inline">{user.displayName}</h1> : <><h1 className="inline">Guest</h1></>}!</h1>
-                {user?.photoURL ? <img src={user.photoURL} alt="User Profile Picture" className="w-32 rounded-lg shadow-lg border-4 dark:border-white hover:border-primaryBlue-primary transition-all" /> : <img src="https://novagoncdn.netlify.app/img/guest_pfp.png" alt="Guest Profile Picture" className="w-32 rounded-lg shadow-lg border-4 dark:border-white hover:border-primaryBlue-primary transition-all" />}
+                {user?.photoURL ? <img src={user.photoURL} alt="User Profile Picture" className="w-32 transition-all border-4 rounded-lg shadow-lg dark:border-white hover:border-primaryBlue-primary" /> : <img src="https://novagoncdn.netlify.app/img/guest_pfp.png" alt="Guest Profile Picture" className="w-32 transition-all border-4 rounded-lg shadow-lg dark:border-white hover:border-primaryBlue-primary" />}
             </div>
             <div>
                 <h2 className="text-2xl">Info</h2>
@@ -30,6 +30,14 @@ function Profile() {
                 </div>
                 }
             </div>
+        </>
+    )
+}
+
+function EditProfile(){
+    return(
+        <>
+
         </>
     )
 }
